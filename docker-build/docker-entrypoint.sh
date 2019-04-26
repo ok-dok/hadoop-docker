047e48b1ac62287f2261c6b46f32ed8f43883b6c
Shawn Hou <shawn_hou@163.com>
2019-04-26 15:47:32 +0800

    修改env环境变量

--- docker-build/docker-entrypoint.sh
+++ docker-build/docker-entrypoint.sh
@@ -6,7 +6,7 @@ gosu hadoop echo N | exec gosu hadoop $HADOOP_HOME/bin/hdfs namenode -format >>/
 gosu hadoop $HADOOP_HOME/sbin/start-dfs.sh
 gosu hadoop $HADOOP_HOME/sbin/start-yarn.sh
 
-if [ "$1" = "-d" ]; then
+if [ "$1" = "-log" ]; then
   tail -f $HADOOP_HOME/logs/hadoop-hadoop-datanode-*.log $HADOOP_HOME/logs/hadoop-hadoop-namenode-*.log 
 fi
 
