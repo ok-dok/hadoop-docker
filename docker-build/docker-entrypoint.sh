5ce9bad7660321d73d4d8aeb49c67cd791ed9a2e
Shawn Hou <shawn_hou@163.com>
2019-05-06 16:11:54 +0800

    修改dockerfile和entrypoint.sh脚本，增加环境变量文件，使得hadoop用户也能访问环境变量

--- docker-build/docker-entrypoint.sh
+++ docker-build/docker-entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 set -ex
 
-service ssh start
-gosu hadoop echo N | exec gosu hadoop $HADOOP_HOME/bin/hdfs namenode -format >>/dev/null
-gosu hadoop $HADOOP_HOME/sbin/start-dfs.sh
-gosu hadoop $HADOOP_HOME/sbin/start-yarn.sh
+. /etc/profile >/dev/null  2>&1
 
-if [ "$1" = "-log" ]; then
+service ssh start
+
+# for worker in $HADOOP_WORKER_NAMES; do
+#   echo "$worker" >> $HADOOP_CONF_DIR/workers
+# done
+
+if [ "$1" = "--start-cluster" ]; then
+  gosu hadoop echo N | gosu hadoop hdfs namenode -format
+  gosu hadoop $HADOOP_HOME/sbin/start-dfs.sh
+  gosu hadoop $HADOOP_HOME/sbin/start-yarn.sh
   tail -f $HADOOP_HOME/logs/hadoop-hadoop-datanode-*.log $HADOOP_HOME/logs/hadoop-hadoop-namenode-*.log 
 fi
 
-if [ "$1" = "-bash" ]; then
-  /bin/bash
+if [ "$1" = "-d" ]; then
+  while true; do sleep 1000; done
 fi
 
 exec "$@"
---
+++ docker-build/hadoop_profile.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/docker-java-home/jre
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
+export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH
