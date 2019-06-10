f8a608ce951bd095b3007dc25e8b7730799880d9
shawn_hou <shawn_hou@163.com>
2019-06-10 15:32:47 +0800

    modify java_home path, add classpath env

--- docker-build/hadoop_profile.sh
+++ docker-build/hadoop_profile.sh
@@ -1,4 +1,5 @@
-export JAVA_HOME=/docker-java-home/jre
+export JAVA_HOME=/usr/local/openjdk-8
+export CLASSPATH=.:$JAVA_HOME/lib
 export HADOOP_HOME=/opt/hadoop
 export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
 export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH
